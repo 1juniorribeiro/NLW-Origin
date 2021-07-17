ce3d6da42448b16d1c6ba0a371b901dc459b6ea0
Amauri Junior <junior_18ribeiro@hotmail.com>
2021-07-17 16:49:51 -0400

    final class

--- main.js
+++ main.js
@@ -19,10 +19,9 @@ for (const link of links) {
 
 // header shadow when scroll the page
 
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
-
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -38,7 +37,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 // ScrollReveal
@@ -52,9 +57,9 @@ const scrollReveal = ScrollReveal({
 
 scrollReveal.reveal(
   ` 
-  #home .image ,#home .text,
-  #about .image, #about .text,
-  #services header, #services .card,
+  #home .text, #home .image,
+  #about .text, #about .image,
+  #services header, #services .cards,
   #testimonials header, #testimonials .testimonials
   #contact .text, #contact .links
   footer .brand, footer .social
@@ -64,13 +69,37 @@ scrollReveal.reveal(
 
 // button back to top
 
+const backToTopButton = document.querySelector('.back-to-top')
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
-
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
+  }
+}
+
+// active menu with section into the page
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
   }
 }
 
@@ -79,4 +108,5 @@ function backToTop() {
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
